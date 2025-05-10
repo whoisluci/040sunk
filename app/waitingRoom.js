bd3f1c1151c96753f0ab766e3784ba8ccad9ef80
whoisluci <lucmov99@gmail.com>
2025-05-10 14:52:48 +0200

    Creation of waiting room

---
+++ app/waitingRoom.js
@@ -0,0 +1,57 @@
+import { PubSub } from "../utils/pubSub";
+import { STATE } from "..";
+
+function renderWaitingRoom(parentID) {
+    const header = renderHeader('#wrapper');
+
+    const codeDiv = document.createElement('div');
+    codeDiv.id = 'codeDiv';
+    document.querySelector(parentID).append(codeDiv);
+
+    const code = document.createElement('p');
+    code.id = 'code';
+    codeDiv.append(code);
+    code.innerText = `#${STATE.team.id}`;
+    
+    const headline = document.createElement('h3');
+    headline.id = 'headline';
+    headline.innerText = 'Väntar...'
+    document.querySelector(parentID).append(headline);
+
+    const playersDiv = document.createElement('div');
+    playersDiv.id = 'playersDiv';
+    document.querySelector(parentID).append(playersDiv);
+
+    /* loopa igenom alla spelare och rendera namn */
+    for (let player of STATE.players ) {
+        const name = renderPlayerName(player.name);
+    }
+
+    const startBttn = document.createElement('button');
+    startBttn.id = 'startBttn';
+    startBttn.innerText = 'START';
+    document.querySelector(parentID).append(startBttn);
+
+    if (STATE.team.creator !== STATE.clientID) {
+        startBttn.disabled = true;
+    }
+    startBttn.addEventListener('click', () => {
+        const data = {
+            event: 'startGame',
+            data: {
+                clientID: STATE.clientID,
+                teamID: STATE.teamID
+            }
+        };
+
+        STATE.socket.send(JSON.stringify(data));
+    });
+}
+
+export function renderPlayerName (name) {
+    const playersDiv = document.querySelector('#playersDiv');
+    const playerName = document.createElement('h4');
+    playerName.id = name;
+    playerName.textContent = name;
+    playersDiv.append(playerName);
+}
