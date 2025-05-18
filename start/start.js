6b239e7158bbdc6c350995ff015aada4887e7b64
whoisluci <lucmov99@gmail.com>
2025-05-18 15:01:46 +0200

    Updated landingPage

--- start/start.js
+++ start/start.js
@@ -1,7 +1,6 @@
 import { STATE } from "../index.js";
 import { PubSub } from "../utils/pubSub.js";
 import * as renderRegister from './register/register.js';
-import * as renderLogIn from './logIn/login.js';
 
 PubSub.subscribe({
     event: 'renderLandingPage',
@@ -9,15 +8,34 @@ PubSub.subscribe({
 });
 
 export function renderLandingPage(parentID) {
-    const logotype = document.createElement('img');
-    document.querySelector(parentID).append(logotype);
-    logotype.id = 'logotype';
-    logotype.src = '../../assets/logotype.png';
+    const background = document.createElement('img');
+    document.querySelector(parentID).append(background);
+    background.id = 'backgroundImg';
+    background.src = '../assets/bgMap.png';
+
+    const logotypeDiv = document.createElement('div');
+    document.querySelector(parentID).append(logotypeDiv);
+    logotypeDiv.id = 'logotypeDiv';
+
+    const beer = document.createElement('img');
+    beer.id = 'beer';
+    logotypeDiv.append(beer);
+    beer.src = '../../assets/beer.png';
+
+    const logo = document.createElement('img');
+    logo.id = 'logotypeText';
+    logotypeDiv.append(logo);
+    logo.src = '../../assets/logotype.png';
 
     const tagline = document.createElement('h2');
     tagline.id = 'tagline';
     document.querySelector(parentID).appendChild(tagline);
     tagline.innerText = 'tagline'; /* lägg in tagline */
+
+    const logInText = document.createElement('h3');
+    logInText.id = 'logInText';
+    document.querySelector(parentID).append(logInText);
+    logInText.textContent = 'Logga in';
 
     const inputDiv = document.createElement('div');
     document.querySelector(parentID).appendChild(inputDiv);
@@ -25,7 +43,9 @@ export function renderLandingPage(parentID) {
 
     inputDiv.innerHTML = `
         <input type='text' name='username' id='username' placeholder='Användarnamn'/>
+        <img id='userIcon' class='inputIcon' src='../assets/icons/user.svg'>
         <input type='password' name='lösenord' id='password' placeholder='Lösenord'/>
+        <img id='lockIcon' class='inputIcon' src='../assets/icons/lock.svg'>
     `;
     
     const logInBttn = document.createElement('button');
@@ -34,10 +54,14 @@ export function renderLandingPage(parentID) {
     document.querySelector(parentID).append(logInBttn)
     logInBttn.innerText = 'Logga in';
 
+    const line = document.createElement('hr');
+    line.id = 'line';
+    document.querySelector(parentID).append(line);
+
     const registerLine = document.createElement('h5');
     registerLine.id = 'register';
     document.querySelector(parentID).append(registerLine);
-    registerLine.innerHTML = `Ny här? <a id='registerBttn'>Registrera dig</a>`;
+    registerLine.innerHTML = `Ny här? <a id='registerLink'>Registrera dig</a>`;
 
     logInBttn.addEventListener('click', () => {
         const name = document.querySelector('#username').value;
@@ -54,7 +78,7 @@ export function renderLandingPage(parentID) {
         STATE.socket.send(JSON.stringify(data));
     });
 
-    const registerBttn = document.querySelector('#registerBttn');
+    const registerBttn = document.querySelector('#registerLink');
     
     registerBttn.addEventListener('click', () => {
        PubSub.publish({
